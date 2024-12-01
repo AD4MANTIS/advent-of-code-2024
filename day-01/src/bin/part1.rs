e49d032ab786b5ef5bef88dad8b564a5baf94041
AD4MANTIS <31745687+AD4MANTIS@users.noreply.github.com>
2024-12-01 11:07:43 +0100

    Day 1 Part 1

--- day-01/src/bin/part1.rs
+++ day-01/src/bin/part1.rs
@@ -1,7 +1,30 @@
 #![allow(unused_variables)]
 
-lib::day!(00, part1, example => 0);
+use lib::ToVec;
+
+lib::day!(00, part1, example => 11, answer => 765748);
 
 fn part1(input: &str) -> usize {
-    todo!()
+    let location_ids = input
+        .lines()
+        .flat_map(|line| {
+            line.split_whitespace().map(|location_id| {
+                location_id
+                    .parse::<usize>()
+                    .expect("Should be a valid Number")
+            })
+        })
+        .to_vec();
+
+    let mut list_1 = location_ids.iter().cloned().step_by(2).to_vec();
+    let mut list_2 = location_ids.into_iter().skip(1).step_by(2).to_vec();
+
+    list_1.sort();
+    list_2.sort();
+
+    list_1
+        .into_iter()
+        .zip(list_2)
+        .map(|ids| ids.0.abs_diff(ids.1))
+        .sum()
 }
