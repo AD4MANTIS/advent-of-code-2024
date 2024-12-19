03dda32e74fc937056776a85d1f5a38ecae62bf5
AD4MANTIS <31745687+AD4MANTIS@users.noreply.github.com>
2024-12-19 20:07:49 +0100

    Day 19 Part 1 example

--- day-19/src/part1.rs
+++ day-19/src/part1.rs
@@ -1,5 +1,36 @@
-lib::day!(19, part1,);
+use lib::ToVec;
 
-fn part1(_input: &str) -> usize {
-    todo!()
+lib::day!(19, part1, example => 6, answer => 0);
+
+fn part1(input: &str) -> usize {
+    let towels = input.lines().next().unwrap().split(", ").to_vec();
+
+    let designs = input
+        .lines()
+        .skip_while(|line| !line.is_empty())
+        .skip(1)
+        .to_vec();
+
+    designs
+        .into_iter()
+        .filter(|design| can_design_be_arranged(&towels, design))
+        .count()
+}
+
+fn can_design_be_arranged(towels: &[&str], design: &str) -> bool {
+    if design.is_empty() {
+        return true;
+    }
+
+    for towel in towels {
+        if !design.starts_with(*towel) {
+            continue;
+        }
+
+        if can_design_be_arranged(towels, &design[towel.len()..]) {
+            return true;
+        }
+    }
+
+    false
 }
