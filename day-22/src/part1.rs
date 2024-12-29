485bc4fecb7fdf398acad5277ead34af2a127e67
AD4MANTIS <31745687+AD4MANTIS@users.noreply.github.com>
2024-12-29 18:41:06 +0100

    Day 22 Part 1

--- day-22/src/part1.rs
+++ day-22/src/part1.rs
@@ -1,5 +1,36 @@
-lib::day!(22, part1,);
+use std::usize;
 
-fn part1(_input: &str) -> usize {
-    todo!()
+lib::day!(22, part1, example => 37327623, answer => 0);
+
+fn part1(input: &str) -> usize {
+    input
+        .lines()
+        .map(|line| line.parse::<usize>().unwrap())
+        .map(|mut secret| {
+            for _ in 0..2000 {
+                secret = calc_next_secret(secret);
+            }
+
+            secret
+        })
+        .sum()
+}
+
+fn calc_next_secret(mut secret: usize) -> usize {
+    secret = mix(secret * 64, secret);
+    secret = prune(secret);
+    secret = mix(secret / 32, secret);
+    secret = prune(secret);
+    secret = mix(secret * 2048, secret);
+    secret = prune(secret);
+
+    secret
+}
+
+const fn mix(secret: usize, num: usize) -> usize {
+    secret ^ num
+}
+
+const fn prune(secret: usize) -> usize {
+    secret % 16777216
 }
