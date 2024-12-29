aed1ea476582a368692ca611e3ba86994b720bd1
AD4MANTIS <31745687+AD4MANTIS@users.noreply.github.com>
2024-12-29 16:59:19 +0100

    Day 21 Part 1

--- day-21/src/part1.rs
+++ day-21/src/part1.rs
@@ -1,26 +1,17 @@
 use std::iter::repeat_n;
 
-use lib::ToVec;
-
-lib::day!(21, part1, example => 126384, answer => 0);
+lib::day!(21, part1, example => 126384, answer => 242484);
 
 fn part1(input: &str) -> usize {
     input
         .lines()
         .map(|line| {
             let code = line.chars().filter_map(|c| NumericKeypad::try_from(c).ok());
-            let shortes_sequence = find_shortes_sequence(code);
-            let shortes_sequence = find_shortes_sequence(shortes_sequence);
-            let shortes_sequence = find_shortes_sequence(shortes_sequence);
-
-            {
-                let sequence = shortes_sequence.clone().to_vec();
-                println!("{line} (len = {1}): {0:?}", sequence, sequence.len());
-            }
+            let shortes_sequence = find_shortes_sequence_len(code, 2);
 
             let code_num = line[..3].parse::<usize>().unwrap();
 
-            shortes_sequence.count() * code_num
+            shortes_sequence * code_num
         })
         .sum()
 }
@@ -59,8 +50,12 @@ impl Keypad for NumericKeypad {
         }
     }
 
-    fn row_movement_first(self) -> bool {
-        self.row() != 0
+    fn can_make_row_movement_first(self, to: Self) -> bool {
+        self.row() != 0 || to.col() != 0
+    }
+
+    fn can_make_col_movement_first(self, to: Self) -> bool {
+        self.col() != 0 || to.row() != 0
     }
 }
 
@@ -135,8 +130,12 @@ impl Keypad for DirectionalKeypad {
         }
     }
 
-    fn row_movement_first(self) -> bool {
-        self.row() == 0
+    fn can_make_row_movement_first(self, to: Self) -> bool {
+        self.row() == 0 || to.col() != 0
+    }
+
+    fn can_make_col_movement_first(self, to: Self) -> bool {
+        self.col() != 0 || to.row() == 0
     }
 }
 
@@ -164,9 +163,13 @@ impl TryFrom<char> for DirectionalKeypad {
 trait Keypad: Copy + Default + TryFrom<char, Error = ()> {
     fn row(self) -> u8;
     fn col(self) -> u8;
-    fn row_movement_first(self) -> bool;
+    fn can_make_row_movement_first(self, to: Self) -> bool;
+    fn can_make_col_movement_first(self, to: Self) -> bool;
 
-    fn input_sequence_to(self, to: Self) -> impl Iterator<Item = DirectionalKeypad> {
+    fn input_sequence_to(
+        self,
+        to: Self,
+    ) -> impl Iterator<Item = impl Iterator<Item = DirectionalKeypad>> {
         type Dir = DirectionalKeypad;
 
         let col_diff = to.col() as isize - self.col() as isize;
@@ -181,11 +184,15 @@ trait Keypad: Copy + Default + TryFrom<char, Error = ()> {
             col_diff.unsigned_abs(),
         );
 
-        if self.row_movement_first() {
-            row_movement.chain(col_movement)
-        } else {
-            col_movement.chain(row_movement)
-        }
+        [
+            self.can_make_row_movement_first(to)
+                .then(|| row_movement.clone().chain(col_movement.clone())),
+            self.can_make_col_movement_first(to)
+                .then(|| col_movement.chain(row_movement)),
+        ]
+        .into_iter()
+        .flatten()
+        .map(|seq| seq.chain([DirectionalKeypad::Activate]))
     }
 }
 
@@ -194,19 +201,24 @@ struct Robot<T: Keypad> {
     pub position: T,
 }
 
-fn find_shortes_sequence<T: Keypad>(
-    code: impl Iterator<Item = T>,
-) -> impl Iterator<Item = DirectionalKeypad> + Clone {
+fn find_shortes_sequence_len<T: Keypad>(code: impl Iterator<Item = T>, depth: usize) -> usize {
     let mut robot = Robot::<T>::default();
 
     code.flat_map(|c| {
-        let sequence = robot
+        let complexity = robot
             .position
             .input_sequence_to(c)
-            .chain([DirectionalKeypad::Activate]);
+            .map(|seq| {
+                if depth == 0 {
+                    seq.count()
+                } else {
+                    find_shortes_sequence_len(seq, depth - 1)
+                }
+            })
+            .min();
+
         robot.position = c;
-        sequence
+        complexity
     })
-    .to_vec()
-    .into_iter()
+    .sum()
 }
